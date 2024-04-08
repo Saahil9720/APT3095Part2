90ab6a1da40c19ced4bdc464ff6c1be7be87cdba
Saahil9720 <saahilvekariya20@hotmail.com>
2024-04-08 16:59:06 +0300

    index.js updated

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 3000;
 
+// Set the directory that contains the HTML file as a static resource
+app.use(express.static(path.join(__dirname)));
+
+// Redirect the root to index.html explicitly (not needed if index.html is to be the default served file)
 app.get("/", (req, res) => {
-  res.redirect("./index.html");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(port, () => {
